610830ae420ce77882eb449a57687a6b55a5557b
matson <matsonjuniorti@gmail.com>
2023-05-03 19:46:54 -0300

    Criação do Login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuroraController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +14,18 @@ use App\Http\Controllers\AuroraController;
 |
 */
 
-Route::get('/', [AuroraController::class,'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/login', [AuroraController::class,'login']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/register',[AuroraController::class,'register']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
