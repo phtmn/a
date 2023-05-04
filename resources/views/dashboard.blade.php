be288b37798ebe58228bf99d975c323eab218d41
matson <matsonjuniorti@gmail.com>
2023-05-04 20:13:08 -0300

    Update login 0.3

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Aurora') }}
+            {{ __('Aurora App') }}
         </h2>
     </x-slot>
 
@@ -9,22 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form action="" style="color: black">
-                        <label for="">User ID :</label><br>
-                        <input  type="text" name="user" id="user"><br><br>
-                        <label for="">Título :</label><br>
-                        <input  type="text" name="titulo" id="titulo"><br><br>
-                        <label for="">Descrição :</label><br>
-                        <input type="text" name="desc" id="desc"><br><br>
-                        <label for="">Imagem :</label><br>
-                        <input type="file" src="" alt=""><br><br>
-                        <label for="">Regulation :</label><br>
-                        <input type="text" name="regu" id="regu"><br><br>
-                        <label for="">Comissão :</label><br>
-                        <input type="text" name="comiss" id="comiss" style="border: black "><br><br><br>
 
-                        <input type="submit" value="Postar">
-                    </form>
+                    <h1>Onde ficara a lista para ser lida , editada e excluida</h1>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ListaController::class,'index']);
+
+Route::get('/lista', [ListaController::class,'show'])->middleware(['auth', 'verified'])->name('lista');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
